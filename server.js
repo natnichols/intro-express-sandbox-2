c604ec54a756c54d18f4bb5042eed36ddb9323f0
NatNichols <natalie.e.nichols@gmail.com>
2024-01-09 22:06:26 -0500

    refactor root route to redirect to /home

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs')
 // mount routes
 
 app.get('/', function(req, res) {
-  res.send(`<h1>Mulder, it's Express</h1>`)
+  res.redirect('/home')
 })
 
 app.get('/home', function(req, res) {
